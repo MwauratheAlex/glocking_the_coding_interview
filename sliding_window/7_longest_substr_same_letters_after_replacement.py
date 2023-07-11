0f1c3c560c2ce4684ddbb30b67f4dc7b0e5ced14
Mwaura Mbugua <mwaura@kali.mwaura>
2023-07-11 14:04:35 +0300

    7_longest_substr_same_letters_after_replacement

--- sliding_window/7_longest_substr_same_letters_after_replacement.py
+++ sliding_window/7_longest_substr_same_letters_after_replacement.py
@@ -5,7 +5,30 @@ no more than ‘k’ letters with any letter, find the length of the longest sub
 having the same letters after replacement."""
 
 def length_of_longest_substring(string, k):
-    return -1
+    window_start = 0
+    max_len = 0
+    max_count = 0
+    freq_map = {}
+
+    #Extend
+    for window_end in range(len(string)):
+        right_char = string[window_end]
+        if right_char not in freq_map:
+            freq_map[right_char] = 0
+        freq_map[right_char] += 1
+        max_count = max(max_count, freq_map[right_char])
+
+        #Shrink
+        if (window_end - window_start + 1 - max_count) > k:
+            left_char = string[window_start]
+            freq_map[left_char] -= 1
+            window_start += 1
+
+        #Update max_len
+        max_len = max(max_len, window_end - window_start + 1)
+
+
+    return max_len
 
 def main():
     print(length_of_longest_substring("aabccbb", k=2))
